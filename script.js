37047154bca28cf6699d15579e9236a794184698
Saksham Verma <vermajisaksham2727@gmail.com>
2026-02-25 10:07:21 +0530

    Primitive Types

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ num = null;
 console.log(num);
 console.log(typeof(num));
 
+const sym1 = Symbol(4);
+console.log(sym1);
+
+const sym2 = Symbol(4);
+console.log(sym2);
+
+if(sym1==sym2){
+    console.log("true");
+}
+
+else{
+    console.log("false"); // because each symbol is unique
+}
+
