9162c60df9c1398100611f79b43044ca47d08d5f
Saksham Verma <vermajisaksham2727@gmail.com>
2026-02-25 09:51:48 +0530

    Primitive Types

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello World")
+var num ;
+num = 10 ;
+console.log(num);
+console.log(typeof(num));
+
+num = "ten" ;
+console.log(num);
+console.log(typeof(num));
+
+num = true ;
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
