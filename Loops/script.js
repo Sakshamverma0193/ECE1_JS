64fa30170ca5b7ce6974abdeeda1f49715deec0c
Saksham Verma <vermajisaksham2727@gmail.com>
2026-03-11 09:20:51 +0530

    Loops Completed

--- Loops/script.js
+++ Loops/script.js
@@ -3,3 +3,13 @@ for( i = 0 ; i < 3 ; i = i + 1 )
 {
     console.log(i);
 } 
+
+const person = {
+    fname: "Ajay",
+    lname: "Singh",
+    age: 25
+
+};
+for(let x in person){
+    console.log("person details:"+ x + ":" + person[x])
+}
